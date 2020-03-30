5bbcca47d0ff0da9cb5df7c229d28a899742c261
kimtype <kimtybe@naver.com>
2020-03-30 16:02:01 +0900

    01 수업과제

---
+++ 01/app.js
@@ -0,0 +1,49 @@
+console.log('app.js');
+
+// var log = function(str){
+//     console.log(str);
+// }
+// log('hello');
+
+
+// function log(str){
+//     console.log(str);
+// }
+// log('hello');
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name:'1.png',
+// };
+
+// // var imageWidth = 100;
+// // var imageHeight = 100;
+// // var imageName = '1.png';
+
+// var image1 = [2,3,4];
+// image1[0] = 'a';
+
+// var data = null;
+// var data1 = '';
+// var data2;
+// console.log(data, data1, data2);
+
+// console.log(image, image1);
+// console.log(1+2);
+// console.log('a'+'b');
+// var count = 1;
+// var num = '1';
+//console.log(count+num);
+// console.log('' === 0);
+
+// var isHero = true;
+// var isHulk = false;
+// if(isHero&&isHulk) {
+//     console.log('hero');
+// }
+
+// console.log(typeof '1');
+// console.log(typeof []);
+
+
