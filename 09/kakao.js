943879f0ab226a65ed3238b1a6651612d4d9752f
kimtype <kimtybe@naver.com>
2020-05-28 20:36:08 +0900

    start fe 10

--- 09/kakao.js
+++ 09/kakao.js
@@ -1,9 +1,23 @@
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
+const $result = document.querySelector('#result');
 const url = 'https://dapi.kakao.com/v2/search/web';
+const headers = { Authorization: 'KakaoAK 72b295819f909ac2f0c3d09eb2f51122' };
 
-function success() {}
-function error() {}
+function success(data) {
+  const { documents } = data;
+  const li = documents.map((doc) => {
+    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
+  });
+
+  $result.innerHTML = `<ul? class="list-group list-group-flush">${li.join(
+    '',
+  )}</ul?`;
+}
+function error() {
+  // eslint=disablic-next-line no-alert
+  alert('데이터를 찾을 수 없습니다.');
+}
 function search() {
   const { value } = $text;
   if (value === '') {
@@ -12,7 +26,10 @@ function search() {
     return false;
   }
 
-  fetch(`${url}?query=${value}`).then(success).catch(error);
+  fetch(`${url}?query=${value}`, { headers })
+    .then((res) => res.json())
+    .then(success)
+    .catch(error);
   return true;
 }
 
